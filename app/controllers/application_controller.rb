1794496fb802135527b9ae8173f6a5cf8ccd12d1
shothree <110705912+shothree@users.noreply.github.com>
2022-08-06 12:26:04 +0900

    ini

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def cafe
+        render html:"appへようこそいらさいませ"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get '/' >= "home#top"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      root 'home#top'
 end
